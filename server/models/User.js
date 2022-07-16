6ae25f75034e14f090139561f21c69c8cc45f7ee
orlinromy <orlinromy@gmail.com>
2022-07-16 17:25:32 +0800

    completed registration + login + create fridge

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     hash: { type: String, required: true },
     fridgeId: [String],
