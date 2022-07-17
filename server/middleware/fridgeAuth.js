8c8e8c311584dba73df0e03ebaa0013d7411f69e
orlinromy <orlinromy@gmail.com>
2022-07-17 12:13:26 +0800

    completed update item in fridge and rename adminAuth to fridgeAuth

---
+++ server/middleware/fridgeAuth.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+const Fridge = require("../models/Fridge");
+
+const fridgeAuth = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].replace("Bearer ", "");
+
+    if (token) {
+      const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+
+      const targetFridge = req.body.fridgeId;
+
+      const fridgeAdmin = await Fridge.findOne({ _id: targetFridge }).select(
+        "admin members"
+      );
+
+      if (fridgeAdmin.length === 0) {
+        return res
+          .status(400)
+          .json({ status: 400, message: "couldn't find fridge" });
+      }
+
+      if (decoded.id === fridgeAdmin.admin) {
+        req.decoded = decoded;
+        req.decoded.admin = true;
+        console.log(decoded);
+        next();
+      } else if (fridgeAdmin.members.includes(decoded.id)) {
+        req.decoded = decoded;
+        req.decoded.admin = false;
+        console.log(decoded);
+        next();
+      } else {
+        return res.status(403).json({ status: 403, message: "not authorized" });
+      }
+    } else {
+      return res.status(403).json({ status: 403, message: "missing token" });
+    }
+  } catch (error) {
+    return res.status(400).json({ status: 400, message: error.message });
+  }
+};
+
+module.exports = fridgeAuth;
--- server/middleware/memberAuth.js
+++ server/middleware/memberAuth.js
@@ -1,19 +1,24 @@
 const jwt = require("jsonwebtoken");
 
-const memberAuth = (req, res, next) => {
+const memberAuth = async (req, res, next) => {
   try {
     const token = req.headers["authorization"].replace("Bearer ", "");
 
     if (token) {
       const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
-      if (!decoded.admin) {
+      const targetFridge = req.body.fridgeId;
+
+      const fridgeAdmin = await Fridge.findOne({ _id: targetFridge }).select(
+        "members"
+      );
+      if (fridgeAdmin.members.includes(decoded.id)) {
         req.decoded = decoded;
         next();
       } else {
         return res.status(403).json({ status: 403, message: "not authorized" });
       }
     } else {
-      return res.status(403).json({ status: 403, message: "missing token " });
+      return res.status(403).json({ status: 403, message: "missing token" });
     }
   } catch (error) {
     return res.status(401).send({ status: "error", message: error.message });
